77d5086bdedb8b7ea2838a68bfcb43308270a1c0
zehenghou <hzh15191415002@163.com>
2023-11-23 23:05:02 +0800

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+}
+
+android {
+    namespace = "com.aaliezl.master.tetris"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.aaliezl.master.tetris"
+        minSdk = 24
+        //noinspection OldTargetApi
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    // Common
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("com.google.android.material:material:1.10.0")
+    // firebase
+    implementation("com.google.firebase:firebase-crashlytics:18.6.0")
+    implementation("com.google.firebase:firebase-analytics:21.5.0")
+
+    // Compose
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose")
+    implementation("androidx.compose.material:material")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
---
+++ app/src/main/java/com/aaliezl/master/tetris/logic/Brick.kt
@@ -0,0 +1,25 @@
+package com.aaliezl.master.tetris.logic
+
+import androidx.compose.ui.geometry.Offset
+
+data class Brick(val location: Offset = Offset(0, 0)) {
+    companion object {
+        private fun of(offsetList: List<Offset>) = offsetList.map { Brick(it) }
+
+        fun of(spirit: Spirit) = of(spirit.location)
+
+        fun of(xRange: IntRange, yRange: IntRange) =
+            of(mutableListOf<Offset>().apply {
+                xRange.forEach { x ->
+                    yRange.forEach { y ->
+                        this += Offset(x, y)
+                    }
+                }
+            })
+
+    }
+
+    fun offsetBy(step: Pair<Int, Int>) =
+        copy(location = Offset(location.x + step.first, location.y + step.second))
+
+}
